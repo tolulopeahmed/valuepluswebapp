f17aa48ac0abcaef74af4f72ce880c05ee01d289
Tolulope Ahmed <tolulopeahmed@gmail.com>
2026-07-15 07:49:40 +0100

    Updated

---
+++ src/app/app/more/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Title from "../../../components/Title";
+import Subtitle from "../../../components/Subtitle";
+
+export default function MorePage() {
+  return (
+    <div className="vp-card-in">
+      <Title className="block">More</Title>
+      <Subtitle>Profile and settings</Subtitle>
+    </div>
+  );
+}
---
+++ src/app/app/withdraw/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Title from "../../../components/Title";
+import Subtitle from "../../../components/Subtitle";
+
+export default function MorePage() {
+  return (
+    <div className="vp-card-in">
+      <Title className="block">Withdraw</Title>
+      <Subtitle>Move money from your account to your bank account</Subtitle>
+    </div>
+  );
+}
